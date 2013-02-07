8cffb83c5106636061f02c09d441a676d9e32786
ericzon <ericzon@gmail.com>
2013-02-07 16:56:55 +0100

    Primer commit del repo remoto

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8" />
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+      <title>titulo</title>
+      <?php
+      
+      ?>
+    </head>
+    <body>
+  <header>  
+    <h1>titulo</h1>
+    <!-- Header content -->
+    
+  </header>
+  <div role="main">
+    <!-- Page content -->
+    <section>
+    	<article>
+    
+    	</article>
+    </section>
+  </div>
+  <aside role="complementary">
+    <!-- Secondary content -->
+    
+    <?php
+
+    ?>
+  </aside>
+  <footer>
+    <!-- Footer content -->
+  </footer>
+</body>
+</html>
